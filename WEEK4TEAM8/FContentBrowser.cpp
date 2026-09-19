9351e7c72900caeb6da82c34b069158a7d3f9fb4
Jonggon Kim <kimjg0105@gmail.com>
2026-09-19 21:03:08 +0900

    feat: 하단 바로 브라우저 이동

---
+++ WEEK4TEAM8/FContentBrowser.cpp
@@ -0,0 +1,230 @@
+#include "FContentBrowser.h"
+#include "ImGui/imgui_internal.h"
+
+void FContentBrowser::Initialize(const std::filesystem::path& InitDirectory)
+{
+	RootDirectory = InitDirectory;
+	CurrentDirectory = InitDirectory;
+}
+
+void FContentBrowser::SetEventHandler(FContentBrowserEventHandler* InEventHandler)
+{
+	EventHandler = InEventHandler;
+}
+
+/*void FContentBrowser::Render()
+{
+	struct FAssetFileEntry
+	{
+		FAssetFileHeader Header;
+		std::filesystem::path FilePath;
+	};
+
+	TArray<FAssetFileEntry> NewAssetFiles;
+	TArray<std::filesystem::path> DeletedAssetFiles;
+
+	ImGui::Begin("Content Browser");
+
+	if (ImGui::Button("Import Texture2D"))
+	{
+		std::filesystem::path TargetPath;
+		if (FNativeFileDialog::OpenFileDialog(CurrentDirectory, { FFileFilter{ L"Image Files", L"*.png;*.jpg;" } }, L"", TargetPath))
+		{
+			FImagePayload ImagePayload;
+			if (FImageFileIO::Load(TargetPath, ImagePayload))
+			{
+				std::filesystem::path NewFilePath = CurrentDirectory / (TargetPath.stem().string() + ".uasset");
+				// TODO: 파일 이름이 중복되는 경우 처리 필요
+
+				FWindowsBinWriter FileWriter(NewFilePath);
+
+				FAssetFileHeader Header;
+				Header.Version = 1;
+				Header.AssetType = EAssetType::Texture2D;
+				Header.AssetID = FGuid::NewGuid();
+
+				FileWriter << Header;
+				if (FImageFileIO::Save(FileWriter, ImagePayload))
+				{
+					NewAssetFiles.Emplace(FAssetFileEntry{ Header, NewFilePath });
+				}
+			}
+		}
+	}
+
+	ImGui::Separator();
+
+	ImGui::Text("Current Directory: %s", CurrentDirectory.string().c_str());
+
+	for (const auto& Entry : std::filesystem::directory_iterator(CurrentDirectory))
+	{
+		if (Entry.is_directory())
+		{
+			if (ImGui::Button(Entry.path().filename().string().c_str()))
+			{
+				CurrentDirectory = Entry.path();
+			}
+		}
+		else
+		{
+			std::filesystem::path Extension = Entry.path().extension();
+			if (Extension == ".uasset")
+			{
+				std::filesystem::path FileName = Entry.path().filename();
+				ImGui::Text("%s", FileName.string().c_str());
+			}
+		}
+	}
+
+	if (ImGui::Button("<-"))
+	{
+		if (CurrentDirectory != RootDirectory)
+		{
+			CurrentDirectory = CurrentDirectory.parent_path();
+		}
+	}
+
+	ImGui::End();
+
+	// Dispatch events for new and deleted asset files
+	if (EventHandler)
+	{
+		for (const auto& NewFile : NewAssetFiles)
+		{
+			EventHandler->OnNewAssetFile(NewFile.Header, NewFile.FilePath);
+		}
+
+		for (const auto& DeletedFile : DeletedAssetFiles)
+		{
+			EventHandler->OnDeleteAssetFile(DeletedFile);
+		}
+	}
+}*/
+
+void FContentBrowser::Render()
+{
+	if (ImGui::IsKeyDown(ImGuiKey_LeftCtrl) && ImGui::IsKeyPressed(ImGuiKey_Space, false))
+	{
+		ToggleDrawer();
+	}
+
+	RenderBottomBar();
+
+	if (bIsDrawerOpen)
+	{
+		RenderDrawer();
+	}
+}
+
+void FContentBrowser::RenderBottomBar()
+{
+	const ImGuiViewport* Viewport = ImGui::GetMainViewport();
+
+	ImGui::SetNextWindowPos(ImVec2(Viewport->WorkPos.x, Viewport->WorkPos.y + Viewport->WorkSize.y - BottomBarHeight));
+	ImGui::SetNextWindowSize(ImVec2(Viewport->WorkSize.x, BottomBarHeight));
+	ImGui::SetNextWindowViewport(Viewport->ID);
+
+	const ImGuiWindowFlags BottomBarFlags =
+		ImGuiWindowFlags_NoDecoration |
+		ImGuiWindowFlags_NoMove |
+		ImGuiWindowFlags_NoResize |
+		ImGuiWindowFlags_NoSavedSettings |
+		ImGuiWindowFlags_NoDocking |
+		ImGuiWindowFlags_NoScrollbar;
+
+	ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+	ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(6.0f,3.0f));
+	ImGui::PushStyleColor(ImGuiCol_WindowBg, IM_COL32(24, 24, 24, 255));
+
+	if (ImGui::Begin("##EditorBottomBar", nullptr, BottomBarFlags))
+	{
+		if (bIsDrawerOpen)
+		{
+			ImGui::PushStyleColor(ImGuiCol_Button, IM_COL32(230, 120, 0, 255));
+		}
+		else
+		{
+			ImGui::PushStyleColor(ImGuiCol_Button, IM_COL32(45, 45, 48, 255));
+		}
+
+		if (ImGui::Button("[ Content Drawer] (Ctrl+Space)"))
+		{
+			ToggleDrawer();
+		}
+
+		ImGui::PopStyleColor();
+
+		ImGui::SameLine();
+		ImGui::SetCursorPosX(ImGui::GetWindowWidth() - 250.0f);
+		ImGui::TextDisabled("Path: %s", CurrentDirectory.filename().string().c_str());
+	}
+
+	ImGui::End();
+
+	ImGui::PopStyleColor();
+	ImGui::PopStyleVar(2);
+}
+
+void FContentBrowser::RenderDrawer()
+{
+	const ImGuiViewport* Viewport = ImGui::GetMainViewport();
+
+
+	const ImVec2 DrawerPos = { Viewport->WorkPos.x, Viewport->WorkPos.y + Viewport->WorkSize.y - BottomBarHeight - DrawerHeight };
+	const ImVec2 DrawerSize = { Viewport->WorkSize.x, DrawerHeight };
+
+	ImGui::SetNextWindowPos(DrawerPos, ImGuiCond_Always);
+	ImGui::SetNextWindowSize(DrawerSize, ImGuiCond_Always);
+	ImGui::SetNextWindowViewport(Viewport->ID);
+
+	const ImGuiWindowFlags DrawerFlags =
+		ImGuiWindowFlags_NoMove |
+		ImGuiWindowFlags_NoCollapse |
+		ImGuiWindowFlags_NoDocking;
+
+	ImGui::PushStyleColor(ImGuiCol_WindowBg, IM_COL32(32, 32, 32, 245));
+
+	if (ImGui::Begin("Content Drawer", &bIsDrawerOpen, DrawerFlags))
+	{
+		if (ImGui::Button("Import Texture2D"))
+		{
+			std::filesystem::path TargetPath;
+			if (FNativeFileDialog::OpenFileDialog(CurrentDirectory, { FFileFilter{ L"Image Files", L"*.png;*.jpg;" } }, L"", TargetPath))
+			{
+				FImagePayload ImagePayload;
+				if (FImageFileIO::Load(TargetPath, ImagePayload))
+				{
+					std::filesystem::path NewFilePath = CurrentDirectory;
+					FWindowsBinWriter FileWriter(NewFilePath);
+
+					FAssetFileHeader Header;
+					Header.Version = 1;
+					Header.AssetType = EAssetType::Texture2D;
+					Header.AssetID = FGuid::NewGuid();
+
+					FileWriter << Header;
+					if (FImageFileIO::Save(FileWriter, ImagePayload))
+					{
+						if (EventHandler) EventHandler->OnNewAssetFile(Header, NewFilePath);
+					}
+				}
+			}
+		}
+
+		ImGui::SameLine();
+		ImGui::Text(" | Current: %s", CurrentDirectory.string().c_str());
+
+		ImGui::SameLine(ImGui::GetWindowWidth() - 70.0f);
+		if (ImGui::Button("Close"))
+		{
+			bIsDrawerOpen = false;
+		}
+
+		ImGui::Separator();
+
+		//if()
+	}
+	ImGui::End();
+
+	ImGui::PopStyleColor();
+}
--- WEEK4TEAM8/FContentBrowser.h
+++ WEEK4TEAM8/FContentBrowser.h
@@ -14,109 +14,24 @@ struct FContentBrowserEventHandler
 class FContentBrowser
 {
 public:
-	void Initialize(const std::filesystem::path& InitDirectory)
-	{
-		RootDirectory = InitDirectory;
-		CurrentDirectory = InitDirectory;
-	}
+	void Initialize(const std::filesystem::path& InitDirectory);
+	void SetEventHandler(FContentBrowserEventHandler* InEventHandler);
 
-	void SetEventHandler(FContentBrowserEventHandler* InEventHandler)
-	{
-		EventHandler = InEventHandler;
-	}
+	void Render();
 
-	void Render()
-	{
-		struct FAssetFileEntry
-		{
-			FAssetFileHeader Header;
-			std::filesystem::path FilePath;
-		};
+	void ToggleDrawer() { bIsDrawerOpen = !bIsDrawerOpen; }
+	bool IsDrawerOpen() const { return bIsDrawerOpen; }
 
-		TArray<FAssetFileEntry> NewAssetFiles;
-		TArray<std::filesystem::path> DeletedAssetFiles;
-
-		ImGui::Begin("Content Browser");
-
-		if (ImGui::Button("Import Texture2D"))
-		{
-			std::filesystem::path TargetPath;
-			if (FNativeFileDialog::OpenFileDialog(CurrentDirectory, { FFileFilter{L"Image Files", L"*.png;*.jpg;"} }, L"", TargetPath))
-			{
-				FImagePayload ImagePayload;
-				if (FImageFileIO::Load(TargetPath, ImagePayload))
-				{
-					std::filesystem::path NewFilePath = CurrentDirectory / (TargetPath.stem().string() + ".uasset");
-					// TODO: 파일 이름이 중복되는 경우 처리 필요
-				
-					FWindowsBinWriter FileWriter(NewFilePath);
-				
-					FAssetFileHeader Header;
-					Header.Version = 1;
-					Header.AssetType = EAssetType::Texture2D;
-					Header.AssetID = FGuid::NewGuid();
-
-					FileWriter << Header;
-					if (FImageFileIO::Save(FileWriter, ImagePayload))
-					{
-						NewAssetFiles.Emplace(FAssetFileEntry{ Header, NewFilePath });
-					}
-				}
-			}
-		}
-
-		ImGui::Separator();
-
-		ImGui::Text("Current Directory: %s", CurrentDirectory.string().c_str());
-
-		for (const auto& Entry : std::filesystem::directory_iterator(CurrentDirectory))
-		{
-			if (Entry.is_directory())
-			{
-				if (ImGui::Button(Entry.path().filename().string().c_str()))
-				{
-					CurrentDirectory = Entry.path();
-				}
-			}
-			else
-			{
-				std::filesystem::path Extension = Entry.path().extension();
-				if (Extension == ".uasset")
-				{
-					std::filesystem::path FileName = Entry.path().filename();
-					ImGui::Text("%s", FileName.string().c_str());
-				}
-			}
-		}
-
-		if (ImGui::Button("<-"))
-		{
-			if (CurrentDirectory != RootDirectory)
-			{
-				CurrentDirectory = CurrentDirectory.parent_path();
-			}
-		}
-
-		ImGui::End();
-
-		// Dispatch events for new and deleted asset files
-		if (EventHandler)
-		{
-			for (const auto& NewFile : NewAssetFiles)
-			{
-				EventHandler->OnNewAssetFile(NewFile.Header, NewFile.FilePath);
-			}
-
-			for (const auto& DeletedFile : DeletedAssetFiles)
-			{
-				EventHandler->OnDeleteAssetFile(DeletedFile);
-			}
-		}
-	}
+private:
+	void RenderBottomBar();
+	void RenderDrawer();
 
 private:
 	std::filesystem::path RootDirectory;
 	std::filesystem::path CurrentDirectory;
-
 	FContentBrowserEventHandler* EventHandler = nullptr;
+
+	bool bIsDrawerOpen = false;
+	float DrawerHeight = 350.0f;
+	const float BottomBarHeight = 28.0f;
 };
