f2a934758f5709e6fa95ad71bba861e3ff1885ed
Jonggon Kim <kimjg0105@gmail.com>
2026-09-21 13:34:20 +0900

    fix: 스태틱 메쉬 컴포넌트 메테리얼을 섹션으로 받도록 수정

--- WEEK4TEAM8/UStaticMeshComponent.cpp
+++ WEEK4TEAM8/UStaticMeshComponent.cpp
@@ -76,7 +76,8 @@ void UStaticMeshComponent::Render(FRenderCollector& RenderCollector)
     for (int32 SectionIndex = 0; SectionIndex < mMeshAsset->GetSections().Num(); ++SectionIndex)
     {
         const FStaticMeshSection& Section = mMeshAsset->GetSections()[SectionIndex];
-        TSharedPtr<FMaterialAsset> Material = FAssetManager::Get().GetAssetAs<FMaterialAsset>(Section.MaterialAssetID, true);
+
+        TSharedPtr<FMaterialAsset> Material = mMaterialAssets[SectionIndex];
 
         // 콤보에서 고른 게 있으면 그 material 적용, 없으면 기존 material 사용
         const FVector4 MaterialColor = Material
