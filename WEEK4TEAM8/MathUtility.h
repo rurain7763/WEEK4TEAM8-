fe636d483061c3f7596e42173a7896f2455eaf17
Jonggon Kim <kimjg0105@gmail.com>
2026-09-22 20:22:41 +0900

    fix: 크기가 작을 때 피킹 안되는 문제 해결

--- WEEK4TEAM8/MathUtility.h
+++ WEEK4TEAM8/MathUtility.h
@@ -5,7 +5,7 @@
 
 constexpr float PI = 3.1415926535897932f;
 constexpr double DOUBLE_PI = 3.1415926535897932;
-constexpr float SMALL_NUMBER = 1.e-8f;
+constexpr float SMALL_NUMBER = 1.e-37f;
 constexpr float KINDA_SMALL_NUMBER = 1.e-4f;
 
 // [[nodiscard]] -> 반환값을 버리면 경고 표시
